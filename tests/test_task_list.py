172a966965dd5333d2d00663c61b91d0a8997b9d
adalberto.vieira <adalbertov@ciandt.com>
2023-05-21 14:31:54 -0300

    add new completion back end logic for task

--- tests/test_task_list.py
+++ tests/test_task_list.py
@@ -16,7 +16,64 @@ def test_unique_id_created_with_new_task(task_list):
     
     
 def test_complete_task_succesfully(task_list):
-    """ """
+    """ test the completion of a task """
+    task_list.create_id = MagicMock(return_value=0)
     task_list.creat_task("test1", "testing")
+    task_list.create_id = MagicMock(return_value=1)
     task_list.creat_task("test2", "testing")
-    pass
+    task_list.complete_task(1)
+    assert task_list.get_tasks() == { 
+        0:{
+            "completed":False,
+            "description":"testing",
+            "title":"test1"
+        },
+        1:{
+           "completed":True,
+            "description":"testing",
+            "title":"test2" 
+        }
+    }
+    
+def test_get_completed_tasks(task_list):
+    """ test the completion of a task """
+    task_list.create_id = MagicMock(return_value=0)
+    task_list.creat_task("test1", "testing")
+    task_list.create_id = MagicMock(return_value=1)
+    task_list.creat_task("test2", "testing")
+    task_list.create_id = MagicMock(return_value=2)
+    task_list.creat_task("test3", "testing")
+    task_list.complete_task(1)
+    task_list.complete_task(2)
+    assert task_list.get_completed_task() == { 
+        1:{
+           "completed":True,
+            "description":"testing",
+            "title":"test2" 
+        },
+        2:{
+           "completed":True,
+            "description":"testing",
+            "title":"test3" 
+        }
+    }
+    
+def test_get_uncompleted_tasks(task_list):
+    """ test the completion of a task """
+    task_list.create_id = MagicMock(return_value=0)
+    task_list.creat_task("test1", "testing")
+    task_list.create_id = MagicMock(return_value=1)
+    task_list.creat_task("test2", "testing")
+    task_list.create_id = MagicMock(return_value=2)
+    task_list.creat_task("test3", "testing")
+    task_list.complete_task(1)
+    task_list.complete_task(2)
+    assert task_list.get_uncompleted_tasks() == { 
+        0:{
+            "completed":False,
+            "description":"testing",
+            "title":"test1"
+        }
+    }
+    
+    
