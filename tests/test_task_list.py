9d102759bf42ef6f1ac358111c56c37a5342dcb9
Wilgnert de Alcântara <wilgnert@hotmail.com>
2023-05-21 14:28:00 -0300

    Testing task editing when title is not provided

--- tests/test_task_list.py
+++ tests/test_task_list.py
@@ -26,9 +26,20 @@ def test_task_should_update_title_when_title_is_edited(task_list):
     new_title:str = "new" + old_title
     task_list.create_id = MagicMock(return_value=0)
 
-    task_list.create_task("test1", "testing")
+    task_list.create_task(old_title, "testing")
     task_list.edit_task(0, title=new_title)
 
     task = task_list.get_task_by_id(id=0)
 
     assert task["title"] == new_title
+
+def test_task_should_not_update_title_when_title_is_not_edited(task_list):
+    title:str = "test1"
+    task_list.create_id = MagicMock(return_value=0)
+
+    task_list.create_task(title, "testing")
+    task_list.edit_task(0)
+
+    task = task_list.get_task_by_id(id=0)
+
+    assert task["title"] == title
